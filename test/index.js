b82cdcd4283fb3bb434a0f33f2f517b0ea169f59
Cameron Bytheway <cameron@nujii.com>
2013-01-17 08:41:55 -0700

    Add test structure

---
+++ test/index.js
@@ -0,0 +1,11 @@
+describe("startup(1)", function() {
+
+  it("should start an express 3.x app");
+  it("should start an express 2.x app");
+  it("should start an connect 2.x app");
+  it("should start an connect 1.x app");
+  it("should start an HTTPServer app");
+
+  it("should handle request errors in domains");
+
+});
